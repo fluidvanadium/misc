02f2d2b876d49834f12d1cb74d95436fbb197d0b
fluidvanadium <fluidvanadium@zingolabs.org>
2024-12-09 22:50:56 +0000

    improving layouts

--- docks/timestamps/moon_phase.py
+++ docks/timestamps/moon_phase.py
@@ -5,8 +5,12 @@ Author: Sean B. Palmer, inamidst.com
 Cf. http://en.wikipedia.org/wiki/Lunar_phase#Lunar_phase_calculation
 """
 
-import math, decimal, datetime
+import sys, math, decimal, datetime
 dec = decimal.Decimal
+
+offset = dec("0.20439731")
+# the lunations at jan 1, 2001
+lunations_per_day = dec("0.03386319269")
 
 def position(now=None): 
    if now is None: 
@@ -14,13 +18,11 @@ def position(now=None):
 
    diff = now - datetime.datetime(2001, 1, 1)
    days = dec(diff.days) + (dec(diff.seconds) / dec(86400))
-   lunations = dec("0.20439731") + (days * dec("0.03386319269"))
+   lunations = offset + (days * lunations_per_day)
 
    return lunations % dec(1)
 
-def phase(pos): 
-   index = (pos * dec(8)) + dec("0.5")
-   index = math.floor(index)
+def phase(index): 
    return {
       0: "New Moon", 
       1: "Waxing Crescent", 
@@ -32,12 +34,33 @@ def phase(pos):
       7: "Waning Crescent"
    }[int(index) & 7]
 
-def main(): 
-   pos = position()
-   phasename = phase(pos)
+def next_quarter(): 
+   now = datetime.datetime.now()
+   
+   diff = now - datetime.datetime(2001, 1, 1)
+   days = dec(diff.days) + (dec(diff.seconds) / dec(86400))
+   lunations = offset + (days * lunations_per_day)
+   
+   quarter_type = phase(math.ceil(lunations * 4) * 2)
+   next_quarter = math.ceil(lunations * 4)
+   # the upcoming quarter is the nth since 2001.
+   lunations = dec(next_quarter / 4.) - offset
+   days = lunations / lunations_per_day
+   
+   date_of_quarter = datetime.datetime(2001, 1, 1) + datetime.timedelta(days=float(days))
+   return (date_of_quarter, quarter_type)
 
-   roundedpos = round(float(pos), 3)
-   print("%s (%s)" % (phasename, roundedpos))
+def main(): 
+   if len(sys.argv) == 1:
+      pos = position()
+
+      roundedpos = round(float(pos), 3)
+      print("%s" % (roundedpos))
+   else:
+      (date,name) = next_quarter()
+
+      print("%s at %s|%s-%s;%s:%s_%s" % (name, date.year, '{0:02}'.format(date.month), '{0:02}'.format(date.day), '{0:02}'.format(date.hour), '{0:02}'.format(date.minute), '{0:02}'.format(date.second)))
+   
 
 if __name__=="__main__": 
    main()
